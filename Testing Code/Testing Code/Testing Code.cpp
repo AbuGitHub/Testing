1444e8548252a20a8e47eb2f0c122b1d3c6190b7
Zohaib <zkhawaja721@gmail.com>
2016-10-13 09:48:15 -0500

    Spacing for ease of reading

--- Testing Code/Testing Code/Testing Code.cpp
+++ Testing Code/Testing Code/Testing Code.cpp
@@ -5,8 +5,10 @@
 int add(int x, int y)
 {
 	using namespace std;
+
 	int z = x + y;
 	cout << x << " plus " << y << " is " << z << endl;
+
 	return z;
 }
 
@@ -14,8 +16,10 @@ int add(int x, int y)
 int subtract(int x, int y)
 {
 	using namespace std;
+
 	int z = x - y;
 	cout << x << " minus " << y << " is " << z << endl;
+
 	return z;
 } 
 
@@ -23,8 +27,10 @@ int subtract(int x, int y)
 int multiply(int x, int y)
 {
 	using namespace std;
+
 	int z = x * y;
 	cout << x << " times " << y << " is " << z << endl;
+
 	return z;
 }
 
@@ -32,9 +38,11 @@ int multiply(int x, int y)
 int divide(int x, int y)
 {
 	using namespace std;
+
 	int z = x / y;
 	int w = x % y;
 	cout << x << " divided by " << y << " is " << z << " with a remainder of " << w << endl;
+
 	return z;
 }
 
@@ -43,13 +51,19 @@ int main()
 {
 	int x;
 	int y;
+
 	std::cout << "Please enter the 2 numbers you want to add together:" << std::endl;
 	std::cin >> x;
 	std::cin >> y;
 	std::cout << "Thank you, please hold on while your numbers are added..." << std::endl;
+
 	add(x, y);
+
 	subtract(x, y);
+
 	multiply(x, y);
+
 	divide(x, y);
+
 	return 0;
 }
