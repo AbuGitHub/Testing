3394f5018d8658fcd9aed6a8df78f65c3b695458
AbuGitHub <abuzar.noorali@hotmail.com>
2016-10-21 19:21:51 -0500

    Major revamp #1
    Major changes: 
    Added do-while loop to determine user input and which operation to do.
    Added skip lines to functions.
    Added a description to inform user of program function.
    Removed std::x and replaced with short form. (namespace std)

--- Testing Code/Testing Code/Testing Code.cpp
+++ Testing Code/Testing Code/Testing Code.cpp
@@ -1,48 +1,37 @@
-#include "stdafx.h"
 #include <iostream>
+using namespace std;
+
 //Abu test (pull branch)
 // Addition
 int add(int x, int y)
 {
-	using namespace std;
-
 	int z = x + y;
-	cout << x << " plus " << y << " is " << z << endl;
-
+	cout << x << " plus " << y << " is " << z << "\n\n";
 	return z;
 }
 
 //Subtraction
 int subtract(int x, int y)
 {
-	using namespace std;
-
 	int z = x - y;
-	cout << x << " minus " << y << " is " << z << endl;
-
+	cout << x << " minus " << y << " is " << z << "\n\n";
 	return z;
-} 
+}
 
 //Multiiplication
 int multiply(int x, int y)
 {
-	using namespace std;
-
 	int z = x * y;
-	cout << x << " times " << y << " is " << z << endl;
-
+	cout << x << " times " << y << " is " << z << "\n\n";
 	return z;
 }
 
 //Division
 int divide(int x, int y)
 {
-	using namespace std;
-
 	int z = x / y;
 	int w = x % y;
-	cout << x << " divided by " << y << " is " << z << " with a remainder of " << w << endl;
-
+	cout << x << " divided by " << y << " is " << z << " with a remainder of " << w << "\n\n";
 	return z;
 }
 
@@ -51,19 +40,50 @@ int main()
 {
 	int x;
 	int y;
+	int input; 	//Holds user input for which function to perform
+	cout << "This is a calculator program." << "\n\n\n";
 
-	std::cout << "Please enter the 2 numbers you want to add together:" << std::endl;
-	std::cin >> x;
-	std::cin >> y;
-	std::cout << "Thank you, please hold on while your numbers are added..." << std::endl;
+	do
+	{
+		cout << "Press 1 to add, 2 to subtract, 3 to multiply, 4 to divide, 0 to quit: " << endl;
+		cin >> input;
 
-	add(x, y);
+		if (input == 1)
+		{
+			cout << "Please enter the 2 numbers you want to add together:" << endl;
+			cin >> x;
+			cin >> y;
+			cout << "Thank you, please hold on while your numbers are added..." << "\n\n";
 
-	subtract(x, y);
+			add(x, y);
+		}
 
-	multiply(x, y);
+		if (input == 2)
+		{
+			cout << "Please enter the 2 numbers you want to subtract together:" << endl;
+			cin >> x;
+			cin >> y;
+			subtract(x, y);
+		}
 
-	divide(x, y);
+		if (input == 3)
+		{
+			cout << "Please enter the 2 numbers you want to multiply:" << endl;
+			cin >> x;
+			cin >> y;
+			multiply(x, y);
+		}
 
+		if (input == 4)
+		{
+			cout << "Please enter the 2 numbers you want to divide:" << endl;
+			cin >> x;
+			cin >> y;
+			divide(x, y);
+		}
+
+	} while (input != 0);
+	
+	cout << endl;
 	return 0;
 }
